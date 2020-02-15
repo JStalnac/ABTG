c2208e7693070efa70a0f5bcd9cf3232d55e4435
JStalnac <55840834+JStalnac@users.noreply.github.com>
2020-02-15 18:57:23 +0200

    Edit prefab and script

--- A ball throwing game/Assets/Scripts/RigidbodyMovement.cs
+++ A ball throwing game/Assets/Scripts/RigidbodyMovement.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class RigidbodyMovement : MonoBehaviour
 {
+    public float speed = 400f;
+    public bool Grounded { get; set; }
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundLayer;
+
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Grounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundLayer);
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        rb.AddRelativeForce(x * speed * Time.deltaTime, 0f, z * speed * Time.deltaTime);
     }
 }
